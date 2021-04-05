2e7563b7fa261e3291808b7127e512c41ae05600
Ahmedkr5 <ahmed.khiari2@esprit.tn>
2021-04-05 18:07:04 +0100

    add Friendlist

--- novant-react/src/components/Friends/FriendReq.js
+++ novant-react/src/components/Friends/FriendReq.js
@@ -1,94 +1,94 @@
 import React from 'react';
-import { withStyles,Typography } from '@material-ui/core';
+import { withStyles, Typography } from '@material-ui/core';
 import Badge from '@material-ui/core/Badge';
-import { makeStyles,MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-import { green,blue } from '@material-ui/core/colors';
-import List from '@material-ui/core/List';
+import { green} from '@material-ui/core/colors';
+
 import Req from './Req'
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#1877F2',
-        }
-      },
+  palette: {
+    primary: {
+      main: '#1877F2',
+    }
+  },
 });
 
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display:'flex',
-        
-      },
-    
-    avatar: {
-        backgroundColor: green[500],
-      },
-    
-      rounded: {
-        color: '#fff',
-        height: '40px',
-        width: '40px',
-        backgroundColor: green[500],
-        cursor: 'pointer',
-    },
-    margin: {
-        margin: theme.spacing(1),
-      },
-      extendedIcon: {
-        marginRight: theme.spacing(1),
-      },
+  root: {
+    display: 'flex',
+
+  },
+
+  avatar: {
+    backgroundColor: green[500],
+  },
+
+  rounded: {
+    color: '#fff',
+    height: '40px',
+    width: '40px',
+    backgroundColor: green[500],
+    cursor: 'pointer',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
 }));
 
 
 const StyledBadge = withStyles((theme) => ({
-    badge: {
-        left : '190%',
-        top: '50%',
-        
-      border: '3px solid',
-      borderColor:'#F8FAFB',
-      padding: '0px',
-      borderRadius :'15px',
-      width: theme.spacing(4),
+  badge: {
+    left: '190%',
+    top: '50%',
+
+    border: '3px solid',
+    borderColor: '#F8FAFB',
+    padding: '0px',
+    borderRadius: '15px',
+    width: theme.spacing(4),
     height: theme.spacing(4),
-    },
-   
-  }))(Badge);
+  },
 
-  //Onclick on badge ,colors,remove caps on buttons
+}))(Badge);
+
+//Onclick on badge ,colors,remove caps on buttons
 
 
 
-export default function  FriendReq() {
-    const classes = useStyles();
-   
-return(<>
-    <div className={classes.root}  style={{marginTop:'10px',flexDirection:'column'}}>
-    <MuiThemeProvider theme={theme}>
-<div style={{width:'360px'}}>
-      <StyledBadge badgeContent={2} color="primary">
-      <div style={{marginLeft:'-140%'}}>
+export default function FriendReq() {
+  const classes = useStyles();
 
-      <Typography style={{color:'grey',fontSize:'12',fontWeight:'bold'}} >REQUESTS</Typography>
-      </div>
-        </StyledBadge>      
+  return (<>
+    <div className={classes.root} style={{ marginTop: '10px', flexDirection: 'column' }}>
+      <MuiThemeProvider theme={theme}>
+        <div style={{ width: '360px' }}>
+          <StyledBadge badgeContent={2} color="primary">
+            <div style={{ marginLeft: '-140%' }}>
+
+              <Typography style={{ color: 'grey', fontSize: '12', fontWeight: 'bold' }} >REQUESTS</Typography>
+            </div>
+          </StyledBadge>
         </div>
-     
-       
- <Req></Req>
- <Req></Req>
- 
- 
- </MuiThemeProvider>
- </div>
 
-        
-        
-        
-        </>
-);
+
+        <Req></Req>
+        <Req></Req>
+
+
+      </MuiThemeProvider>
+    </div>
+
+
+
+
+  </>
+  );
 
 
 
