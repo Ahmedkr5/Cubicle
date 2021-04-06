d16639fd97f993ca70fb480c0d7a0fdf10a14452
Hamza Safraou <hamza.safraou@esprit.tn>
2021-04-06 23:34:26 +0100

    fixed app.js problem

--- novant-react/src/App.js
+++ novant-react/src/App.js
@@ -1,43 +1,34 @@
 import './App.css';
 import ChatApp from './views/ChatApp';
 import Login from './components/Auth/Login/SignIn';
-import VideoCall from './views/VideoCall'
-import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
+import VideoCall from './views/VideoCall';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Profile from './Profile';
-import Groupe from './Groupe'
+import Groupe from './Groupe';
 
 function App() {
-<<<<<<< HEAD
-  return <div className='App'>
-   
-=======
-  return <div className='App' style={{backgroundColor : '#F0F2F5'}}>
->>>>>>> 720d58c176891971e6d26758f2ee181bd4958b24
-     <Router>
-     <Route path="/profile">
-            <Profile/>
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-<<<<<<< HEAD
-          <Route path="/AppChat">
-            <ChatApp/>
-          </Route>
-          <Route
-path="/video"
-render={(props) => <VideoCall {...props} />}
-></Route>
-=======
-          <Route path="/Groupe">
-            <Groupe/>
-          </Route>
->>>>>>> 720d58c176891971e6d26758f2ee181bd4958b24
-  </Router>
-  </div>;
+  return (
+    <div className='App' style={{ backgroundColor: '#F0F2F5' }}>
+      <Router>
+        <Route path='/profile'>
+          <Profile />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/AppChat'>
+          <ChatApp />
+        </Route>
+        <Route
+          path='/video'
+          render={(props) => <VideoCall {...props} />}
+        ></Route>
+        <Route path='/Groupe'>
+          <Groupe />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
