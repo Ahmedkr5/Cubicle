34cb33d6643ec0f809174bfef7ef8155b7fa82a3
Ahmed Zeghibi <ahmed.zeghibi@esprit.tn>
2021-04-07 04:25:55 +0100

    Added a Business page and many new components

--- novant-react/src/components/Navbar/Navbar.js
+++ novant-react/src/components/Navbar/Navbar.js
@@ -8,7 +8,6 @@ import SearchIcon from '@material-ui/icons/Search';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
-import { shadows } from '@material-ui/system';
 
 
 const useStyles = makeStyles((theme) => ({
