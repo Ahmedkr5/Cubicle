bb1a560c760d5d00dbf0051befbb2327f47b2935
Hamza Safraou <hamza.safraou@esprit.tn>
2021-05-17 18:55:47 +0100

    fixed post and comment dates and app lazy loading

--- novant-react/src/App.js
+++ novant-react/src/App.js
@@ -1,55 +1,66 @@
+import React, { lazy, Suspense } from 'react';
 import './App.css';
-import ChatApp from './views/ChatApp';
-import Login from './components/Auth/SignUp/SignUp';
-import VideoCall from './views/VideoCall';
-import Meet from './views/Meet';
-
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Profile from './Profile';
-import Groupe from './Groupe';
-import Home from './Home';
-import GroupProfile from './components/Groups/GroupProfile';
-import Business from './Business';
-import Particulier from './components/Auth/SignUp/Particulier';
+const ChatApp = React.lazy(() => import('./views/ChatApp'));
+const Login = React.lazy(() => import('./components/Auth/SignUp/SignUp'));
+const VideoCall = React.lazy(() => import('./views/VideoCall'));
+const Meet = React.lazy(() => import('./views/Meet'));
 
-
+const Profile = React.lazy(() => import('./Profile'));
+const Groupe = React.lazy(() => import('./Groupe'));
+const Home = React.lazy(() => import('./Home'));
+const GroupProfile = React.lazy(() =>
+  import('./components/Groups/GroupProfile')
+);
+const Business = React.lazy(() => import('./Business'));
+const Particulier = React.lazy(() =>
+  import('./components/Auth/SignUp/Particulier')
+);
 
 function App() {
   return (
-    <div className='App' style={{ backgroundColor: '#F0F2F5' }}>
-      <Router>
-      <Route path='/profile/:id' render={(props) => {
-                    return ( <Profile {...props } /> )
-                }} />
-        <Route path='/GroupProfile/:id' render={(props) => {
-                    return ( <GroupProfile {...props } /> )
-                }} />
-        <Route path='/auth'>
-          <Login />
-        </Route>
-        <Route path='/AppChat'>
-          <ChatApp />
-        </Route>
-        <Route
-          path='/video/:userck'
-          render={(props) => <VideoCall {...props} />}
-        ></Route>
-        <Route
-          path='/meet/:userck'
-          render={(props) => <Meet {...props} />}
-        ></Route>
-        <Route path='/Groupe'>
-          <Groupe />
-        </Route>
-        <Route path='/Home'>
-          <Home />
-        </Route>
-       
-        <Route path='/Business'>
-          <Business />
-        </Route>
-      </Router>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className='App' style={{ backgroundColor: '#F0F2F5' }}>
+        <Router>
+          <Route
+            path='/profile/:id'
+            render={(props) => {
+              return <Profile {...props} />;
+            }}
+          />
+          <Route
+            path='/GroupProfile/:id'
+            render={(props) => {
+              return <GroupProfile {...props} />;
+            }}
+          />
+          <Route path='/auth'>
+            <Login />
+          </Route>
+          <Route path='/AppChat'>
+            <ChatApp />
+          </Route>
+          <Route
+            path='/video/:userck'
+            render={(props) => <VideoCall {...props} />}
+          ></Route>
+          <Route
+            path='/meet/:userck'
+            render={(props) => <Meet {...props} />}
+          ></Route>
+          <Route path='/Groupe'>
+            <Groupe />
+          </Route>
+          <Route path='/Home'>
+            <Home />
+          </Route>
+
+          <Route path='/Business'>
+            <Business />
+          </Route>
+        </Router>
+      </div>
+    </Suspense>
   );
 }
 
