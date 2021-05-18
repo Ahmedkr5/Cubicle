69c356c0f5dd6398b8ee26435c3fe3624068f89b
Ahmedkr5 <ahmed.khiari2@esprit.tn>
2021-05-18 16:15:35 +0100

    add clipboardcopy (very advanced)😎

--- novant-react/src/components/Groups/About.js
+++ novant-react/src/components/Groups/About.js
@@ -6,13 +6,15 @@ import InfoTwoToneIcon from '@material-ui/icons/InfoTwoTone';
 import AccessTimeTwoToneIcon from '@material-ui/icons/AccessTimeTwoTone';
 
 import VisibilityTwoToneIcon from '@material-ui/icons/VisibilityTwoTone';
+import Clipboardbutton from './clipboardbutton';
 export default function About(props) {
 
   return (
     <div >
     <Container style={{borderRadius: '10px'  ,backgroundColor:"white",height:'400px'}}>
     <Row style={{height:'400px'}}>
-    <div style={{marginTop:'40px'}}><h4 style={{color:'blue',fontFamily:'Times New Roman',fontWeight:'bold'}}>About this group</h4><hr></hr>
+    <div style={{marginTop:'40px'}}><h4 style={{color:'blue',fontFamily:'Times New Roman',fontWeight:'bold'}}>About this group</h4><Clipboardbutton></Clipboardbutton>
+    <hr></hr>
      
    
      
@@ -33,6 +35,7 @@ export default function About(props) {
       <Typography variant='h6'component='h6'>
       Description
       </Typography>{props?.desc}
+      
     </div>
     
 
